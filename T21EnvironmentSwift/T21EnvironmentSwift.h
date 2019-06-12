ef7f54bba324cdcf572a0582db0cf2217b26b8f6
Marcos Molero <marcos.molero@worldline.com>
2019-06-12 16:16:37 +0200

    Added T21EnvironmentSwift Framework

---
+++ T21EnvironmentSwift/T21EnvironmentSwift.h
@@ -0,0 +1,19 @@
+//
+//  T21EnvironmentSwift.h
+//  T21EnvironmentSwift
+//
+//  Created by tempos21 on 12/06/2019.
+//  Copyright © 2019 Worldline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for T21EnvironmentSwift.
+FOUNDATION_EXPORT double T21EnvironmentSwiftVersionNumber;
+
+//! Project version string for T21EnvironmentSwift.
+FOUNDATION_EXPORT const unsigned char T21EnvironmentSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <T21EnvironmentSwift/PublicHeader.h>
+
+
